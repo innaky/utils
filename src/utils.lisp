cf472795be5e8d5e8d730cd5f99ac4f646e82e3b
Innaky <innaky@protonmail.com>
2019-05-14 13:39:08 -0400

    (nw fn) random letters and numbers Returning only letters and numbers.

--- src/utils.lisp
+++ src/utils.lisp
@@ -1,6 +1,7 @@
 (defpackage utils
   (:use :cl)
-  (:export :random-string))
+  (:export :random-string
+	   :random-letters-and-numbers))
 
 (in-package :utils)
 
@@ -12,6 +13,12 @@
                "0123456789"
                "!@#$%&/()=*-_.,;{}+[]"))
 
+(defparameter *letters-numbers*
+  (concatenate 'string
+	       "abcdefghijklmnopqrstuvwxyz"
+	       "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	       "0123456789"))
+
 (defun random-element (lst)
   (aref lst (random (length lst))))
 
@@ -21,5 +28,14 @@
       (cons (random-element *characters*)
 	    (random-password (- length 1)))))
 
+(defun random-num-letters (length)
+  (if (equal length 0)
+      nil
+      (cons (random-element *letters-numbers*)
+	    (random-num-letters (- length 1)))))
+
 (defun random-string (length)
-    (format nil "~{~a~}" (random-password length)))
+  (format nil "~{~a~}" (random-password length)))
+
+(defun random-letters-and-numbers (length)
+  (format nil "~{~a~}" (random-num-letters length)))
